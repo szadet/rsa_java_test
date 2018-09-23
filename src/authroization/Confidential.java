192564801ff38be4e44bd62c2fe1b686fce8312d
szadet <szade.tomasz@gmail.com>
2018-09-23 17:12:27 +0200

    Update Confidential.java

--- src/authroization/Confidential.java
+++ src/authroization/Confidential.java
@@ -2,6 +2,6 @@ package authroization;
 public class Confidential {
 	public static final String dbClassName = "com.mysql.jdbc.Driver";
 	public static final String CONNECTION = "jdbc:mysql://127.0.0.1/account";
-	public static final String dbUser = "root";
-	public static final String dbPassword = "rarytas123!";
+	public static final String dbUser = "";
+	public static final String dbPassword = "";
 }
